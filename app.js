d0c9614b02cfdd02314c2f3446106b20a22aecf2
Jonathan Flowers <jonny_flowers@me.com>
2015-10-06 11:27:25 +0100

    Updated logging

--- app.js
+++ app.js
@@ -69,29 +69,29 @@ app.post('/', function(req, res) {
 				io.sockets.emit(req.session.id, {filesize: filesize, progress: progress});
 			})
 			.on('end', function() {
-				console.log('successfully uploaded!');
+				logger.info('successfully uploaded!');
 				fullName = charStrip(fields[0][1])
 				fileType = charStrip(fields[1][1])
 				originalFileName = (files[0][1].name)
 				strippedFileName = charStrip(files[0][1].name)
 				newFileName = uploadDir + '/' + fullName + '_' + fileType + '_' + originalFileName
-				console.log('fullName = ' + fullName);
-				console.log('fileType = ' + fileType);
-				console.log('fileName = ' + originalFileName);
+				logger.info('fullName = ' + fullName);
+				logger.info('fileType = ' + fileType);
+				logger.info('fileName = ' + originalFileName);
 				fs.rename(files[0][1].path, newFileName, function (err) {
 					if (err){
-						console.log('err = ' + err);
+						logger.error('err = ' + err);
 					}
 				});
 				res.render('upload', {fields: fields, files: files});
 			});
 		form.parse(req, function(){
-			console.log(files[0][1].path)
+			logger.info(files[0][1].path)
 		});
 });
 
 server.listen(app.get('port'), function(){
-	console.log("Express server listening on port " + app.get('port'));
+	logger.info("Express server listening on port " + app.get('port'));
 });
 
 
